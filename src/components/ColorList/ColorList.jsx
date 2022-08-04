358ecd751dd6c4646766458d7f3f07144583b566
dalismiles <dali.iapichino@gmail.com>
2022-08-04 21:37:53 +0200

    chore:sec init

--- src/components/ColorList/ColorList.jsx
+++ src/components/ColorList/ColorList.jsx
@@ -1,17 +1,7 @@
-import ColorCard from "../ColorCard";
 import "./index.css";
 
-function ColorList(color) {
-  console.log(color);
-  return (
-    <div className="ColorList">
-      <ColorCard color={color[0]} />
-      <ColorCard color={color[1]} />
-      <ColorCard color={color[2]} />
-      <ColorCard color={color[3]} />
-      <ColorCard color={color[4]} />
-    </div>
-  );
+function ColorList({ children }) {
+  return <div className="ColorList">{children}</div>;
 }
 
 export default ColorList;
