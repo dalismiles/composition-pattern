fa831c45a8a4407995b5fb966cbe12ed3454e0e5
dalismiles <dali.iapichino@gmail.com>
2022-08-04 21:46:15 +0200

    fix:card bg color and main-title changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <MainContent>
-        <h1 className="MainContent__title">Color palette</h1>
+        <h1 className="MainContent__title">Sandy Beach Color Scheme</h1>
         <img
           className="MainContent__img"
           src={
